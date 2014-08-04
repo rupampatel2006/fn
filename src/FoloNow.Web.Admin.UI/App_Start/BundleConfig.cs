a6c76141d7a91cfed534db95ec170c61a120486f
rupampatel2006 <rupampatel2006@gmail.com>
2014-08-04 18:26:45 -0400

    added bundles

--- src/FoloNow.Web.Admin.UI/App_Start/BundleConfig.cs
+++ src/FoloNow.Web.Admin.UI/App_Start/BundleConfig.cs
@@ -8,6 +8,12 @@ namespace FoloNow.Web.Admin.UI
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularroute").Include(
+                        "~/Scripts/angular-route.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 

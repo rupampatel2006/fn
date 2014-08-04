3da123b2876be5270d84aad56f4b4876222e8e6d
rupampatel2006 <rupampatel2006@gmail.com>
2014-08-04 13:49:33 -0400

    setup

---
+++ FoloNow.web.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FoloNow.web.UI.MvcApplication" Language="C#" %>
---
+++ FoloNow.web.UI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FoloNow.web.UI.Startup))]
+
+namespace FoloNow.web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

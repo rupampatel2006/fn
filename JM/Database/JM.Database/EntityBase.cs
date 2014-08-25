d319a43b69315f52fa9b95d90784bc073eb949b7
rupampatel2006 <rupampatel2006@gmail.com>
2014-08-24 23:11:58 -0400

    untracked changes

---
+++ JM/Database/JM.Database/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿
+namespace JM.Database
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ JM/Database/JM.Database/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace JM.Database.Repository
+{
+    public interface IRepository
+    {
+        T Get<T>(int id) where T : EntityBase;
+        IQueryable<T> GetAll<T>() where T : EntityBase;
+        void SaveOrUpdate<T>(T entity) where T : EntityBase;
+        void Delete<T>(T entity) where T : EntityBase;
+    }
+}
